a7d4c0aa7e182f8c75ad6b5cf0b675dafad89029
Hazel Hawadi <hawadihazel@icloud.com>
2024-11-30 05:53:20 +0000

    Create home page view to show all rooms

--- savanna_hotel/hotel_booking/views.py
+++ savanna_hotel/hotel_booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+get_object_or_404, redirect
+from .models import Room, Booking
+from .forms import BookingForm, AddRoomForm
 
 # Create your views here.
+
+# Home page showing all rooms
+def index(request):
+    rooms = Room.objects.all()
+    return render(request, 'hotel_booking/index.html', {'rooms': rooms})

4a69e8d407c08f952dc29830aaf67ab61a27809b
HazelHawadi <hawadihazel@icloud.com>
2025-04-03 11:56:28 +0100

    Create a Contact me page

--- savanna_hotel/urls.py
+++ savanna_hotel/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static  # Import static
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from hotel_booking import views
 
 urlpatterns = [
@@ -14,4 +15,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path("reviews/", include("reviews.urls")),
+    path("contact/", TemplateView.as_view(template_name="contact_us.html"), name="contact_us"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Serve static files during development
